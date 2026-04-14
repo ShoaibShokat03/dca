6f8c89b6c8e9c6c99ac2df611c35db0dd5dd8a3c
ShoaibShokat03 <shoaibshokat03@gmail.com>
2026-04-14 18:45:20 +0500

    uploaded actual empty project

--- android/android/app/src/main/kotlin/com/drpn/ppmanager/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.drpn.ppmanager
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/shoaib_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shoaib_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
